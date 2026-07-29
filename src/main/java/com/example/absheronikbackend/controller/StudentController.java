010355da65c3bf9b15e8dafc996b8a584533d0a2
EminS17 <sadiqliemin17@gmail.com>
2026-07-29 15:51:38 +0400

    Fix delete error and make admin panel fully responsive

--- src/main/java/com/example/absheronikbackend/controller/StudentController.java
+++ src/main/java/com/example/absheronikbackend/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.absheronikbackend.controller;
 
 import com.example.absheronikbackend.entity.Student;
 import com.example.absheronikbackend.repository.StudentRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +26,16 @@ public class StudentController {
     @GetMapping
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    // --- МЕТОД ДЛЯ УДАЛЕНИЯ УЧЕНИКА ПО ID ---
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        if (studentRepository.existsById(id)) {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok().build(); // Возвращает статус 200 OK
+        } else {
+            return ResponseEntity.notFound().build(); // Возвращает 404, если ID не найден
+        }
     }
 }
